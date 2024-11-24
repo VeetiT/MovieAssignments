b95eccd41a26a20ca3d0d542fc3ffdacb60fd9a7
VeetiT <n3tive00@students.oamk.fi>
2024-11-24 23:47:59 +0200

    Opened server and defined raw endpoints

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+app.listen(3001, () => {
+    console.log('Serveri juoksee!');
+});
+
+
+app.get('/movie', (req, res) => {
+    res.send('Movie test')
+})
+
+app.get('/genre', (req, res) => {
+    res.send('genre test')
+})
+
+app.get('/review', (req, res) => {
+    res.send('review test')
+})
+
+app.get('/user', (req, res) => {
+    res.send('user test')
+})
