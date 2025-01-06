f9330a15b0df472fe61a88e13fbe9b282c37a63d
VeetiT <n3tive00@students.oamk.fi>
2025-01-06 19:46:37 +0200

    Made postgre connection and some more

---
+++ pg_connection.js
@@ -0,0 +1,14 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pgPool = new pg.Pool({
+    user: process.env.PG_USER,
+    password: process.env.PG_PW,
+    database: process.env.PG_DB,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+})
+
+
+export {pgPool};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+//import {pgPool} from './pg_connection.js'
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
@@ -6,6 +7,13 @@ app.use(express.urlencoded({extended: true}));
 app.listen(3001, () => {
     console.log('Serveri juoksee!');
 });
+
+
+try {
+    await pgPool.query('');
+}catch(e){
+    console.log(e.message);
+}
 
 
 app.get('/movie', (req, res) => {
@@ -20,14 +28,7 @@ app.get('/review', (req, res) => {
     res.send('review test')
 })
 
-//app.get('/user', (req, res) => {
- //   res.send('user test')
-//})
+app.get('/user', (req, res) => {
+   res.send('user test')
+})
 
-app.post('/user', (req, res) => {
-
-    let user = req.body;
-    console.log(user)
-
-    res.send();
-}) 
